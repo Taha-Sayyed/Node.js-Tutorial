b85a667d168b18975aa6ce391997d6aee6275940
Taha Sayyed <codetaha786@gmail.com>
2026-01-30 00:06:22 +0530

    docs: demonstrate EventEmitter creation, listeners, and event emission in Node.js

---
+++ 02_Events/events-demo.js
@@ -0,0 +1,37 @@
+const EventEmitter=require('events')
+const eventEmitter=new EventEmitter();
+//console.log(EventEmitter)
+console.log(typeof EventEmitter);//function
+//console.log(eventEmitter)
+console.log(typeof eventEmitter)//Object
+
+//Define event and add listener
+eventEmitter.on("greet",()=>{  //multi-callable event
+    console.log(`Hello! I am listening`);
+});
+
+eventEmitter.emit("greet")
+
+//Another way to add listener to the same event
+const mylistener1=function(username){
+    console.log(`Hello ${username}`);
+    
+}
+const mylistener2=function(username){
+    console.log(`Hello ${username}! How are you?`);
+    
+}
+eventEmitter.on("greet",mylistener1)//multi-callable event
+eventEmitter.emit("greet","Taha Sayyed");
+
+eventEmitter.on("greet",mylistener2)//multi-callable event
+eventEmitter.emit("greet","Taha Sayyed");
+
+//total listener on event "greet"
+console.log(eventEmitter.listeners('greet'));
+
+
+
+
+
+
