4d2f14c6ff455b59f767191cad529ca9b98cb524
Taha Sayyed <codetaha786@gmail.com>
2026-01-30 00:24:05 +0530

    docs: demonstrate extending EventEmitter with a custom class and emitting events

---
+++ 02_Events/event-demo-class.js
@@ -0,0 +1,14 @@
+const EventEmitter=require('events');
+
+class Chat extends EventEmitter{
+    sendMessage(msg){
+        this.emit("messageReceived",msg);
+    }
+}
+
+const chat=new Chat();
+chat.on("messageReceived",(msg)=>{
+    console.log(`New Message: ${msg}`);
+});
+
+chat.sendMessage("Hello Taha")
