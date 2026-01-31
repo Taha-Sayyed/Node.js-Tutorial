3d70ce463a923ec732af5205c3e7fc8d989cf3af
Taha Sayyed <codetaha786@gmail.com>
2026-02-01 00:12:21 +0530

    docs: demonstrate Buffer.from copy behavior and independent mutation in Node.js

---
+++ 04_Buffer/buffer-demo-3.js
@@ -0,0 +1,8 @@
+const { Buffer } = require('node:buffer');
+
+// Creates a new Buffer containing the UTF-8 bytes of the string 'buffer'.
+const buf = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]);
+console.log(buf);//<Buffer 62 75 66 66 65 72>
+console.log(buf.toString());//buffer
+
+
