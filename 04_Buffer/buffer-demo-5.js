e59f68707ce077c5fc4dda53126d9e2bf150d6ee
Taha Sayyed <codetaha786@gmail.com>
2026-02-01 00:18:48 +0530

    docs: demonstrate Buffer encoding differences with utf-8, hex, and latin1

---
+++ 04_Buffer/buffer-demo-5.js
@@ -0,0 +1,11 @@
+const { Buffer } = require('node:buffer');
+
+const buf1 = Buffer.from('this is a tést','utf-8');
+const buf2 = Buffer.from('7468697320697320612074c3a97374', 'hex');
+
+console.log(buf1.toString());
+// Prints: this is a tést
+console.log(buf2.toString());
+// Prints: this is a tést
+console.log(buf1.toString('latin1'));
+// Prints: this is a tÃ©st
