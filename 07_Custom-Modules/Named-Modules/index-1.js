501566e6a065d345e9433277eec7751021e1f59f
Taha Sayyed <codetaha786@gmail.com>
2026-02-05 00:41:38 +0530

    docs: demonstrate CommonJS named exports and require destructuring usage

---
+++ 07_Custom-Modules/Named-Modules/index-1.js
@@ -0,0 +1,6 @@
+const value=require('./math')
+console.log(typeof value);//Object
+
+const {add,sub,mul,div}=require('./math');
+console.log(add(4,4));
+console.log(add(4,4));
---
+++ 07_Custom-Modules/Named-Modules/math.js
@@ -0,0 +1,13 @@
+exports.add=function add(a,b){//named export
+    return a+b;
+}
+exports.sub=function sub(a,b){//named export
+    return a-b;
+}
+exports.mul=function mul(a,b){//named export
+    return a*b;
+}
+exports.div=function div(a,b){//named export
+    return a/b;
+}
+
