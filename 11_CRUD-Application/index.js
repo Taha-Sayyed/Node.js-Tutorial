78accb8685526952a122a0355f972acbb594d965
Taha-Sayyed <codetaha786@gmail.com>
2026-02-09 22:49:28 +0530

    feat: set up Express server with books routes and logger middleware

---
+++ 11_CRUD-Application/index.js
@@ -0,0 +1,17 @@
+require('dotenv/config');
+const express=require('express');
+const app=express();
+const bookrouter=require('./routes/books.routes');
+const PORT=8000;
+const {loggerMiddleware}=require('./middlewares/logger');
+
+//Middleware
+app.use(express.json());
+app.use(loggerMiddleware);
+
+//Routes
+app.use('/books',bookrouter);
+
+app.listen(8000,()=>{
+    console.log(`HTTP server is running on port ${PORT}`);
+})
