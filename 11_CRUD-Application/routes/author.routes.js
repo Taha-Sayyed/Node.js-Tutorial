e08e72b58479d43e12694ad913bd92babd6b1d82
Taha-Sayyed <codetaha786@gmail.com>
2026-02-10 21:35:03 +0530

    feat: add route to fetch all books for a specific author

--- 11_CRUD-Application/routes/author.routes.js
+++ 11_CRUD-Application/routes/author.routes.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
-const {getAllAuthor,getAuthorById,createAuthor}=require('../controllers/author.controller');
+const {getAllAuthor,getAuthorById,createAuthor,getAllBooksByAuthor}=require('../controllers/author.controller');
 
 router.get('/',getAllAuthor);
 router.get('/:id',getAuthorById);
 router.post("/",createAuthor);
+router.get("/:id/books",getAllBooksByAuthor);
 
 module.exports=router;
